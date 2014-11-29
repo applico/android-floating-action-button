079bdd10db454f920185b55e8490118ef9786c26
emmano <emmanuel.ortiguela@gmail.com>
2014-11-29 00:49:20 -0500

    android-5-fix: Made project run on Android fix. Removed Object creation on onDraw and onMeasure(). Project can run on phones now. EO

--- app/src/main/java/applico/android_floating_action_button/views/FabView.java
+++ app/src/main/java/applico/android_floating_action_button/views/FabView.java
@@ -10,57 +10,83 @@ import android.graphics.RectF;
 import android.graphics.drawable.AnimatedStateListDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewOutlineProvider;
 import android.widget.ImageView;
 
 import applico.android_floating_action_button.R;
 
 /**
- * This class is intended to imitate the Material Design Fab Button.  Google indicated that they will not be
+ * This class is intended to imitate the Material Design Fab Button.  Google indicated that they
+ * will not be
  * providing this view as part of a support package or the impending "L" release.
+ *
  * @author Matt Powers
  */
 public class FabView extends ImageView {
-    private static String LOG_TAG = FabView.class.getSimpleName();
 
     //Initailize the object variables
     private Paint mCirclePaint;
-    private Paint mStrokePaint;
-    private RectF mCircleArc;
-    private int mCircleRadius;
-    private int mStartAngle;
-    private int mEndAngle;
-    private int mCircleFillColor;
-    private int mCircleStrokeColor;
-    private int mCircleStrokeWidth;
-    private int mFabSize;
-    private int mScreenDensity;
-    private int mDrawableHeight;
-    private int mDrawableWidth;
-    private int mOffsetLeft;
-    private int mOffsetTop;
-    private Outline mOutline;
 
-    private float mScreenDensityFloat;
+    private Paint mStrokePaint;
+
+    private RectF mCircleArc;
+
+    private int mCircleRadius;
+
+    private int mStartAngle;
+
+    private int mEndAngle;
+
+    private int mCircleFillColor;
+
+    private int mCircleStrokeColor;
+
+    private int mCircleStrokeWidth;
+
+    private int mFabSize;
+
+    private int mScreenDensity;
+
+    private int mDrawableHeight;
+
+    private int mDrawableWidth;
+
+    private int mOffsetLeft;
+
+    private int mOffsetTop;
+
     private Drawable mDrawable;
 
 
     //Default values
     //TODO change this to pull from the dimensions file.
     private static final int DEFAULT_RADIUS = 0;
+
     private static final int DEFAULT_FILL_COLOR = Color.BLACK;
+
     private static final int DEFAULT_STROKE_COLOR = Color.BLUE;
+
     private static final int DEFAULT_START_ANGLE = 0;
+
     private static final int DEFAULT_END_ANGLE = 360;
+
     private static final int DEFAULT_STROKE_WIDTH = 2;
 
     //Default FAB sizes according to the material design
     //docs: http://www.google.com/design/spec/components/buttons.html#buttons-main-buttons
 
     private int DEFAULT_FAB_SIZE = 56;
+
     private int DEFAULT_FAB_MINI_SIZE = 40;
 
     private static final int FAB_SIZE_NORMAL = 0;
-    private static final int FAB_SIZE_MINI= 1;
+
+    private static final int FAB_SIZE_MINI = 1;
+
+    private ViewOutlineProvider provider;
+
+    private RectF rectF;
 
     //TODO - support the context only constructor
     public FabView(Context context) {
@@ -88,7 +114,6 @@ public class FabView extends ImageView {
         super(context, attrs, defStyle);
     }
 
-
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.translate(1, 1);
@@ -96,11 +121,12 @@ public class FabView extends ImageView {
         canvas.drawArc(mCircleArc, mStartAngle, mEndAngle, true, mStrokePaint);
         if (mDrawable != null) {
             //left top right bottom
-            mDrawable.setBounds(mOffsetLeft,mOffsetTop,mDrawableWidth+mOffsetLeft,
+            mDrawable.setBounds(mOffsetLeft, mOffsetTop, mDrawableWidth + mOffsetLeft,
                     mDrawableHeight + mOffsetTop);
             mDrawable.draw(canvas);
         }
-        this.setOutline(mOutline);
+
+        this.setOutlineProvider(provider);
         setClipToOutline(true);
     }
 
@@ -110,33 +136,26 @@ public class FabView extends ImageView {
      * @param heightMeasureSpec
      */
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
-    {
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int measuredWidth = measureWidth(widthMeasureSpec);
 
         //We are using this method to provide a default size based on the layout size
-        if(mCircleRadius == 0)
-        {
-            mCircleRadius = measuredWidth/2;
-            int tempRadiusHeight = measureHeight(heightMeasureSpec)/2;
-            if(tempRadiusHeight < mCircleRadius)
-            {
+        if (mCircleRadius == 0) {
+            mCircleRadius = measuredWidth / 2;
+            int tempRadiusHeight = measureHeight(heightMeasureSpec) / 2;
+            if (tempRadiusHeight < mCircleRadius) {
                 mCircleRadius = tempRadiusHeight;
             }
         }
 
-        int circleDiameter = mCircleRadius * 2 - mCircleStrokeWidth;
-        mCircleArc = new RectF(0, 0, circleDiameter, circleDiameter);
+        mCircleArc = rectF;
         int measuredHeight = measureHeight(heightMeasureSpec);
         setMeasuredDimension(measuredWidth, measuredHeight);
 
     }
 
-
     /**
      * measure the height of the screen
-     * @param measureSpec
-     * @return
      */
     private int measureHeight(int measureSpec) {
         int specMode = MeasureSpec.getMode(measureSpec);
@@ -150,14 +169,10 @@ public class FabView extends ImageView {
         return result;
     }
 
-
     /**
      * measure the width of the screen
-     * @param measureSpec
-     * @return
      */
-    private int measureWidth(int measureSpec)
-    {
+    private int measureWidth(int measureSpec) {
         int specMode = MeasureSpec.getMode(measureSpec);
         int specSize = MeasureSpec.getSize(measureSpec);
         int result = 0;
@@ -169,42 +184,39 @@ public class FabView extends ImageView {
         return result;
     }
 
-
     /**
      * initialize all the custom attributes
      * TODO - some of the paint is not supported by attributes
-     * @param attrs
      */
     public void init(AttributeSet attrs) {
         //Convert the fab sizes to DP
-        mScreenDensity = (int)getResources().getDisplayMetrics().density;
-        mScreenDensityFloat = getResources().getDisplayMetrics().density;
+        mScreenDensity = (int) getResources().getDisplayMetrics().density;
 
-        DEFAULT_FAB_SIZE = DEFAULT_FAB_SIZE * (int)mScreenDensity;
-        DEFAULT_FAB_MINI_SIZE = DEFAULT_FAB_MINI_SIZE * (int)mScreenDensity;
+        DEFAULT_FAB_SIZE = DEFAULT_FAB_SIZE * mScreenDensity;
+        DEFAULT_FAB_MINI_SIZE = DEFAULT_FAB_MINI_SIZE * mScreenDensity;
 
         TypedArray attrsArray = getContext().obtainStyledAttributes(attrs, R.styleable.fab);
         mCircleRadius = attrsArray.getInteger(R.styleable.fab_cRadius, DEFAULT_RADIUS);
         mCircleFillColor = attrsArray.getColor(R.styleable.fab_cFillColor, DEFAULT_FILL_COLOR);
-        mCircleStrokeColor = attrsArray.getColor(R.styleable.fab_cStrokeColor, DEFAULT_STROKE_COLOR);
-        mCircleStrokeWidth = attrsArray.getInteger(R.styleable.fab_cStrokeWidth, DEFAULT_STROKE_WIDTH);
+        mCircleStrokeColor = attrsArray
+                .getColor(R.styleable.fab_cStrokeColor, DEFAULT_STROKE_COLOR);
+        mCircleStrokeWidth = attrsArray
+                .getInteger(R.styleable.fab_cStrokeWidth, DEFAULT_STROKE_WIDTH);
         mFabSize = attrsArray.getInteger(R.styleable.fab_cSize, DEFAULT_RADIUS);
         mDrawable = attrsArray.getDrawable(R.styleable.fab_cDrawable);
 
         mStartAngle = DEFAULT_START_ANGLE;
         mEndAngle = DEFAULT_END_ANGLE;
 
-        if(mDrawable != null) {
+        if (mDrawable != null) {
             mDrawableHeight = mDrawable.getIntrinsicHeight();
             mDrawableWidth = mDrawable.getIntrinsicWidth();
-        }
-        else
-        {
+        } else {
             mDrawableHeight = 0;
             mDrawableWidth = 0;
         }
 
-        if(mCircleRadius == DEFAULT_RADIUS) {
+        if (mCircleRadius == DEFAULT_RADIUS) {
             if (mFabSize == FAB_SIZE_NORMAL) {
                 mCircleRadius = (DEFAULT_FAB_SIZE / 2);
             } else if (mFabSize == FAB_SIZE_MINI) {
@@ -213,158 +225,26 @@ public class FabView extends ImageView {
         }
 
         //Compute the offset for the drawable
-        mOffsetLeft = mCircleRadius - (mDrawableWidth/2);
-        mOffsetTop = mCircleRadius - (mDrawableWidth/2);
-
+        mOffsetLeft = mCircleRadius - (mDrawableWidth / 2);
+        mOffsetTop = mCircleRadius - (mDrawableWidth / 2);
 
         //Set the outline for the elevation attribute
-        mOutline = new Outline();
-        mOutline.setOval(0,0,(mCircleRadius * 2),(mCircleRadius *2));
-
         attrsArray.recycle();
+        final int circleDiameter = mCircleRadius * 2 - mCircleStrokeWidth;
+        rectF = new RectF(0, 0, circleDiameter, circleDiameter);
+        provider = new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                outline.setOval(0, 0, circleDiameter, circleDiameter);
+            }
+        };
         this.bringToFront();
         super.setImageDrawable(mDrawable);
     }
 
     /**
-     * Paint used for the view.
-     * @return
-     */
-    public Paint getPaint()
-    {
-        return mCirclePaint;
-    }
-
-    /**
-     * Set the paint of the circleview
-     * @param p
-     */
-    public void setPaint(Paint p)
-    {
-        mCirclePaint = p;
-        invalidate();
-    }
-
-    /**
-     * Get the stroke paint
-     * @return
-     */
-    public Paint getStrokePaint()
-    {
-        return mStrokePaint;
-    }
-
-    /**
-     * Set the stroke paint on the view
-     * @param p
-     */
-    public void setStrokePaint(Paint p)
-    {
-        mStrokePaint = p;
-        invalidate();
-    }
-
-    /**
-     * set the Circle Arc for the view
-     * @param arc
-     */
-    public void setCircleArc(RectF arc)
-    {
-        mCircleArc = arc;
-        invalidate();
-    }
-
-    /**
-     * Get the circle arc
-     * @return
-     */
-    public RectF getCircleArc()
-    {
-        return mCircleArc;
-    }
-
-    /**
-     * Set the circle radius on the view..have fun creating Pacman
-     * @param r
-     */
-
-    public void setCRadius(int r)
-    {
-        mCircleRadius = r;
-        invalidate();
-    }
-
-    /**
-     * Get the radius of the view
-     * @return
-     */
-    public int getCRadius()
-    {
-        return mCircleRadius;
-    }
-
-    /**
-     * Set the fill color
-     * @param color
-     */
-    public void setCFillColor(int color)
-    {
-        mCircleFillColor = color;
-        mCirclePaint.setColor(mCircleFillColor);
-        invalidate();
-    }
-
-    /**
-     * Get the fill color
-     * @return
-     */
-    public int getCFillColor()
-    {
-        return mCircleFillColor;
-    }
-
-    /**
-     * Get the stroke color
-     * @return
-     */
-    public int getCStrokeColor()
-    {
-        return mCircleStrokeColor;
-    }
-
-    /**
-     * Set the stroke color
-     * @param color
-     */
-    public void setCStrokeColor(int color)
-    {
-        mCircleStrokeColor = color;
-        mStrokePaint.setColor(mCircleStrokeColor);
-        invalidate();
-    }
-
-    /**
-     * Get the stroke width
-     * @return
-     */
-    public int getCStrokeWidth()
-    {
-        return mCircleStrokeWidth;
-    }
-
-    /**
-     * Set the stroke width
-     * @param width
-     */
-    public void setCStrokeWidth(int width)
-    {
-        mCircleStrokeWidth = width;
-        mStrokePaint.setStrokeWidth(mCircleStrokeWidth);
-        invalidate();
-    }
-
-    /**
      * Gets the example drawable attribute value.
+     *
      * @return The example drawable attribute value.
      */
     public Drawable getCDrawable() {
@@ -375,6 +255,7 @@ public class FabView extends ImageView {
     /**
      * Sets the view's drawable attribute value. In the example view, this drawable is
      * drawn above the text.
+     *
      * @param drawable attribute value to use.
      */
     public void setCDrawable(AnimatedStateListDrawable drawable) {
